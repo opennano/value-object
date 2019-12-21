27ad5e372c2a01af15355b92a413f9c6f744b7ad
Nathan Noble <nathan_noble@intuit.com>
2019-12-21 10:50:28 -0800

    clean up a mess

--- jsongen/jsongen/src/test/java/scenariotests/pojos/AmbiguousObject.java
+++
@@ -1,9 +0,0 @@
-package scenariotests.pojos;
-
-import java.io.Serializable;
-
-public class AmbiguousObject {
-
-  @SuppressWarnings("unused") // used only by reflection
-  private Serializable testedField;
-}
