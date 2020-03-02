69ba307243950323fb7d178d8c20528287ba9943
Nathan Noble <nathan_noble@intuit.com>
2020-03-01 16:07:04 -0800

    fix one Java and one Maven warning

--- valuegen/src/main/java/com/github/opennano/valuegen/utils/SubtypeUtil.java
+++ valuegen/src/main/java/com/github/opennano/valuegen/utils/SubtypeUtil.java
@@ -6,7 +6,6 @@ import java.util.Arrays;
 import java.util.Collection;
 import java.util.HashSet;
 import java.util.List;
-import java.util.Objects;
 import java.util.Set;
 import java.util.stream.Collectors;
 
